e13e9f7316c45a4bdc3f9016fa37185dd3b63c36
Mohammed-ahmady <323232391@sha.edu.eg>
2025-06-10 23:46:33 +0300

    Implement more add-to-cart functionality

--- parts/urls.py
+++ parts/urls.py
@@ -8,5 +8,8 @@ urlpatterns = [
     path('', PartListView.as_view(), name='part_list'),
     path('add/<int:part_id>/', views.add_to_cart, name='add_to_cart'),
     path('cart/', views.cart_detail, name='cart_detail'),
-    path('<slug:slug>/', PartDetailView.as_view(), name='part_detail'),  # must come last!
+    path('remove/<int:part_id>/', views.remove_from_cart, name='remove_from_cart'),
+    path('checkout/', views.checkout_page, name='checkout_page'),
+    path('<slug:slug>/', PartDetailView.as_view(), name='part_detail'),
+      
 ]
