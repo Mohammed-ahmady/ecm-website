4ce6a76a3703ed46252b5665c01677daac824db5
Mohammed-ahmady <323232391@sha.edu.eg>
2025-08-10 08:25:39 +0300

    Add image_url field to TruckModel and update templates to use get_image_url method

--- parts/admin.py
+++ parts/admin.py
@@ -16,25 +16,51 @@ class PartImageInline(admin.TabularInline):  # or admin.StackedInline
 @admin.register(TruckModel)
 class TruckModelAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
-    list_display = ('name', 'manufacturer', 'year_range', 'has_image')
+    list_display = ('name', 'manufacturer', 'year_range', 'has_image', 'has_image_url')
     search_fields = ('name', 'manufacturer')
-    fields = ('name', 'slug', 'manufacturer', 'year_range', 'description', 'image')
+    fields = ('name', 'slug', 'manufacturer', 'year_range', 'description', 'image', 'image_url')
     
     def has_image(self, obj):
         return bool(obj.image)
     has_image.boolean = True
     has_image.short_description = 'Has Image'
+    
+    def has_image_url(self, obj):
+        return bool(obj.image_url)
+    has_image_url.boolean = True
+    has_image_url.short_description = 'Has Image URL'
   
 
 @admin.register(Part)
 class PartAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('part_number', 'name')}
-    list_display = ('name', 'part_number', 'category', 'price', 'stock', 'is_active')
+    list_display = ('name', 'part_number', 'category', 'price', 'stock', 'has_image', 'is_active')
     list_filter = ('is_active', 'category', 'truck_models')
     list_editable = ('is_active', 'stock')
     search_fields = ('name', 'part_number')
     filter_horizontal = ('truck_models',)
     inlines = [PartImageInline]
+    
+    def has_image(self, obj):
+        return bool(obj.image or obj.image_url)
+    has_image.boolean = True
+    has_image.short_description = 'Has Image'
+    
+    fieldsets = (
+        ('Basic Information', {
+            'fields': ('name', 'part_number', 'slug', 'description', 'category')
+        }),
+        ('Images', {
+            'fields': ('image', 'image_url'),
+            'description': 'You can either upload an image directly or provide an external URL to an image.'
+        }),
+        ('Inventory', {
+            'fields': ('price', 'stock', 'is_active')
+        }),
+        ('Related', {
+            'fields': ('truck_models',)
+        }),
+    )
 
 
 @admin.register(Inquiry)
